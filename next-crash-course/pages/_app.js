43465823aeb8bcf53567ab1a87b813a4114e6dbe
mosesvk <kaumatule93@gmail.com>
2022-03-15 19:49:07 -0600

    Using a Layout Wrap around the _app.js and applying global styles that way

--- next-crash-course/pages/_app.js
+++ next-crash-course/pages/_app.js
@@ -1,7 +1,18 @@
+
+
+import Layout from '../components/Layout'
+
 import '../styles/globals.css'
+// we can only access global css through HERE. If we want to access styles to the components then we will need to access .module.css
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+  )
 }
 
 export default MyApp
